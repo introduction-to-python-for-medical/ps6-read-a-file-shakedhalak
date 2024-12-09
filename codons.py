efd9bbf7c0906db507511288cd57285bd9c32611
shakedhalak <shakedhalak@gmail.com>
2024-12-09 13:24:36 +0200

    Update codons.pynew

--- codons.py
+++ codons.py
@@ -1,11 +1,11 @@
-def create_codon_dict(file_path)
-    file = open(path; 'r')
-    rows= file.readlines()
-    file.close()
-    dictionary = {}
-    for row in rows:
-        row_cells = row.strip().split('/t')
-        codon = row_cells[0]
-        amino_acid = row_cells[2]
-        dictionary[codon] = amino_acid
-    return dictionary 
+def create_codon_dict(file_path):
+     file = open(path; 'r')
+     rows= file.readlines()
+     file.close()
+     dictionary = {}
+     for row in rows:
+         row_cells = row.strip().split('/t')
+         codon = row_cells[0]
+         amino_acid = row_cells[2]
+         dictionary[codon] = amino_acid
+     return dictionary 
